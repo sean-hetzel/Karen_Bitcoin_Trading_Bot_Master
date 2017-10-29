3e257f1ba4c93660d3673a0b45d12c4f78b9a9b0
Seanhetzel <seanhetzel1@gmail.com>
2017-10-28 20:49:25 -0700

    tk

--- Karen.py
+++ Karen.py
@@ -1,43 +1,92 @@
+# The code for changing pages was derived from: http://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
+# License: http://creativecommons.org/licenses/by-sa/3.0/	
+
 import tkinter as tk
+from tkinter import ttk
 from geminipy import Geminipy
 
-LARGE_FONT = ("Verdana", 12)
+LARGE_FONT= ("Verdana", 12)
 
-class gemini_btc(tk.Tk):
-    def __itit__(self, *args, **kwargs):
+
+class SeaofBTCapp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        
         tk.Tk.__init__(self, *args, **kwargs)
+
+        #tk.Tk.iconbitmap(self,default='clienticon.ico')
+        tk.Tk.wm_title(self, "Sea of BTC Client")
+
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand = True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
+
         self.frames = {}
-        for F in (StartPage, PageOne):
-            frame = StartPage(container, self)
-            self.frames[StartPage] = frame
+
+        for F in (StartPage, PageOne, PageTwo):
+
+            frame = F(container, self)
+            self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
+
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
+
         frame = self.frames[cont]
         frame.tkraise()
+
         
 class StartPage(tk.Frame):
+
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="start page", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-        button1 = tk.Button(slef, text="Visit page 1",
-                            command=lambda: conotroller.show_frame(PageOne))
-        button1.pack()
+        tk.Frame.__init__(self,parent)
+        
+        label = ttk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button = ttk.Button(self, text="Visit Page 1",
+                            command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+        button2 = ttk.Button(self, text="Visit Page 2",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
 
 class PageOne(tk.Frame):
+
     def __init__(self, parent, controller):
-        fk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page One", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-        button1 = tk.Button(slef, text="Back to Home",
-                            command=lambda: conotroller.show_frame(StartPage))
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page One!!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
         button1.pack()
+
+        button2 = ttk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = ttk.Button(self, text="Page One",
+                            command=lambda: controller.show_frame(PageOne))
+        button2.pack()
         
-app = gemini_btc()
+
+
+app = SeaofBTCapp()
 app.mainloop()
