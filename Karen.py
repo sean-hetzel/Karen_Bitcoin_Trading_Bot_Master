afbf90dc223754e5a16ea790ba2440da4c7eae03
Seanhetzel <seanhetzel1@gmail.com>
2017-10-29 22:22:31 -0700

    changed Karen

--- Karen.py
+++ Karen.py
@@ -1,92 +1,34 @@
-# The code for changing pages was derived from: http://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
-# License: http://creativecommons.org/licenses/by-sa/3.0/	
-
 import tkinter as tk
 from tkinter import ttk
-from geminipy import Geminipy
-
-LARGE_FONT= ("Verdana", 12)
-
-
-class SeaofBTCapp(tk.Tk):
-
-    def __init__(self, *args, **kwargs):
-        
-        tk.Tk.__init__(self, *args, **kwargs)
-
-        #tk.Tk.iconbitmap(self,default='clienticon.ico')
-        tk.Tk.wm_title(self, "Sea of BTC Client")
-
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand = True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}
-
-        for F in (StartPage, PageOne, PageTwo):
-
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(StartPage)
-
-    def show_frame(self, cont):
-
-        frame = self.frames[cont]
-        frame.tkraise()
-
-        
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        
-        label = ttk.Label(self, text="Start Page", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-        button = ttk.Button(self, text="Visit Page 1",
-                            command=lambda: controller.show_frame(PageOne))
-        button.pack()
-
-        button2 = ttk.Button(self, text="Visit Page 2",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
+import webbrowser
+import geminipy as Geminipy
 
 
-class PageOne(tk.Frame):
+def doorbell(event):
+    print("doorbell rung")
 
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page One!!", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
+def open_cp(event):
+    webbrowser.open_new_tab("https://cleverprogrammer.com")
 
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
+def open_amazon(event):
+    webbrowser.open_new_tab("https://www.amazon.com/")
 
-        button2 = ttk.Button(self, text="Page Two",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
+window = tk.Tk()
+window.geometry("1920x1080")
+label = ttk.Label(text="Welcome to Karen Cryptocurrency Trading Bot!", font="Verdana")
+label.grid(column = 50, row  = 50)
 
+button = ttk.Button(window, text="Doorbell")
+button.grid(column = 0, row = 1)
 
-class PageTwo(tk.Frame):
+button2 = ttk.Button(window, text="Clever Programmer")
+button2.grid(column = 1, row = 1)
 
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page Two!!!", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
+button3 = ttk.Button(window, text="Amazon")
+button3.grid(column = 2, row = 1)
 
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
+button.bind("<Button-1>", doorbell)
+button2.bind("<Button-1>", open_cp)
+button3.bind("<Button-1>", open_amazon)
 
-        button2 = ttk.Button(self, text="Page One",
-                            command=lambda: controller.show_frame(PageOne))
-        button2.pack()
-        
-
-
-app = SeaofBTCapp()
-app.mainloop()
+window.mainloop()
