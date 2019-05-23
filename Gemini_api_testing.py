56ef6bf25f5750a147715cfafee99924680525e2
unknown <seanhetzel1@gmail.com>
2019-05-22 21:19:17 -0700

    just testing git

--- Gemini_api_testing.py
+++ Gemini_api_testing.py
@@ -1,9 +1,13 @@
+import certifi
 import urllib3
-base_url = "https://api.gemini.com/v1"
-# or, for sandbox
-# base_url = "https://api.sandbox.gemini.com/v1"
-http = urllib3.PoolManager()
+import json
+
+#base_url = "https://api.gemini.com/v1"
+
+base_url = "https://api.sandbox.gemini.com/v1"
+http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 response = http.request("GET", base_url + "/pubticker/btcusd")
+json.loads(response.data.decode("utf-8"))
 #response = base_url + "/pubticker/btcusd"
 print(response.read())
-
+urllib3.HTTPConnectionPool
